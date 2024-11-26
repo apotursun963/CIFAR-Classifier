d0917280a5897c6811ad7cd163e14d030cc2c9c2
Abdullah Tursun <tursunapo333>
2024-11-27 00:36:44 +0300

    will update2

---
+++ sources/DataLoader.py
@@ -0,0 +1,85 @@
+
+from keras._tf_keras.keras.datasets import cifar10
+from keras._tf_keras.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+
+class Cifar10DataLoader:
+    def __init__(self, load_data=False) -> None:
+        self.load_data = load_data
+        self.train_imgs = None
+        self.train_labels = None
+        self.test_imgs = None
+        self.test_labels = None
+        self.classes = [
+            "Airplane", "Automobile", "Bird", "Cat", "Deer", 
+            "Dog", "Frog", "Horse", "Ship", "Truck"
+        ]
+
+    def load(self, load=None):
+        if self.load_data == False:
+            raise ValueError("please set load_data=True to load the data.")
+        
+        elif self.load_data and load == "both":
+            (self.train_imgs, self.train_labels), (self.test_imgs, self.test_labels) = cifar10.load_data()
+            print("Successfully loaded train and test datasets")
+
+        elif self.load_data and load == "train":
+            (self.train_imgs, self.train_labels), (_ , _) = cifar10.load_data()
+            print("Successfully loaded train dataset")
+
+        elif self.load_data and load == "test":
+            (_ , _), (self.test_imgs, self.test_labels) = cifar10.load_data()
+            print("Successfully loaded test dataset")
+
+        else:
+            raise ValueError("Data type must be 'train', 'test' or 'both'")
+    
+    def visualize_images(self, num_imgs, dataset=None):
+        if dataset == "train" and self.train_imgs is None:
+            raise ValueError("Training data is not loaded. Please load training data first.")
+
+        elif dataset == "test" and self.test_imgs is None:
+            raise ValueError("Testing data is not loaded. Please load testing data first.")
+
+        plt.figure(figsize=(10, 5))
+        for i in range(num_imgs):
+            if dataset == "train":
+                index = random.randint(0, len(self.train_imgs) - 1)
+                image = self.train_imgs[index]
+                label = self.train_labels[index]
+            else:
+                index = random.randint(0, len(self.test_imgs) - 1)
+                image = self.test_imgs[index]
+                label = self.test_labels[index]
+            plt.subplot(2, 5, i + 1)
+            plt.imshow(image.reshape(32, 32, 3))
+            plt.title(f"{self.classes[np.argmax(label)]}")
+            plt.axis("off")
+        plt.show()
+    
+    def process_data(self, data=None):
+        if data == "train" and self.load_data:
+            if self.train_imgs is None or self.train_labels is None:
+                raise ValueError("Training data is not loaded. Please load training data first.")
+            self.train_imgs = self.train_imgs.reshape(self.train_imgs.shape[0], 32 * 32 * 3) / 255.0
+            self.train_labels = to_categorical(self.train_labels, num_classes=10)
+            print("Train data pre-processing Complete.")
+
+        elif data == "test" and self.load_data:
+            if self.test_imgs is None or self.test_labels is None:
+                raise ValueError("Testing data is not loaded. Please load testing data first.")
+            self.test_imgs = self.test_imgs.reshape(self.test_imgs.shape[0],  32 * 32 * 3) / 255.0
+            self.test_labels = to_categorical(self.test_labels, num_classes=10)
+            print("Test data pre-processing Complete.")
+
+        else:
+            raise ValueError("Please specify the data type as 'train' or 'test'.")
+    def data_summary(self):
+        pass
+
+    def split_data(self):   ## to validation
+        pass
+
---
+++ tests/test_model.py
@@ -0,0 +1,78 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from sources.DataLoader import Cifar10DataLoader
+from sources.utils import relu, softmax, accuracy, cross_entropy_loss
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+
+
+class TestNeuralNet:        # Test loss: 3.6990 | Test accuracy: %49.3300
+    def __init__(self, num_layers, x_test, y_test, make_test=False) -> None:
+        self.num_layers = num_layers
+        self.x_test = x_test
+        self.y_test = y_test
+        self.make_test = make_test
+        self.classes = [
+            "Airplane", "Automobile", "Bird", "Cat", "Deer", 
+            "Dog", "Frog", "Horse", "Ship", "Truck"
+        ]
+
+    def load_parameters(self):
+        self.weight_lst = []; self.bias_lst = []
+
+        for idx in range(self.num_layers - 1):
+            self.weight_lst.append(np.load(f"output\\checkpoints\\weights\\W{idx + 1}.npy"))
+            self.bias_lst.append(np.load(f"output\\checkpoints\\biases\\B{idx + 1}.npy"))
+        return (self.weight_lst, self.bias_lst)
+
+    def evulate(self):
+        if not self.make_test:
+            raise ValueError("you have to set the make_test=True to execute testing evulation")
+
+        W, B = self.load_parameters()
+    
+        self.layer_outputs = [relu(np.dot(self.x_test, W[0]) + B[0])]
+    
+        for i in range(len(self.weight_lst) - 2):
+            self.layer_outputs.append(relu(np.dot(self.layer_outputs[i], W[i + 1]) + B[i + 1]))
+
+        self.prediction = softmax(np.dot(self.layer_outputs[-1], W[-1]) + B[-1])
+        return (self.prediction)
+
+    def compute_acc_loss(self):
+        acc = accuracy(self.y_test, self.prediction)
+        loss = cross_entropy_loss(self.y_test, self.prediction)
+        print(f"Test loss: {loss:.4f} | Test accuracy: %{acc * 100:.4f}")
+        
+    def visualize_results(self, num_imgs=10):
+        plt.figure(figsize=(8, 6))
+        for idx in range(num_imgs):
+            index = random.randint(0, len(self.x_test) - 1)
+            plt.subplot(2, 5, idx + 1)
+            plt.imshow(self.x_test[index].reshape(32, 32, 3))
+            plt.title(f"true: {self.classes[np.argmax(self.y_test[index])]}\npred: {self.classes[np.argmax([self.prediction[index]])]}")
+            plt.axis("off")
+        plt.tight_layout()
+        plt.show()
+
+# her sınıfta burada böyle çağırmayacaksın scrip.sh olarak yapacaksın bunları yada terminalden ala
+data_loader = Cifar10DataLoader(load_data=True)
+data_loader.load(load="test")
+data_loader.process_data(data="test")
+
+
+testing = TestNeuralNet(
+    num_layers=5, 
+    x_test=data_loader.test_imgs, 
+    y_test=data_loader.test_labels,
+    make_test=True
+)
+
+testing.evulate()
+
+testing.visualize_results()
+
+testing.compute_acc_loss()
